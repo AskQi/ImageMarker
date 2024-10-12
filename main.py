05c713cedd41dfb0ad48fb25f4b44a96c31a91fc
qida <974830507@qq.com>
2024-10-12 23:54:20 +0800

    优化图片压缩及日志结果打印

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from collections import Counter
 
 import rawpy
 import cv2
@@ -27,7 +28,7 @@ class ColoredFormatter(logging.Formatter):
 
 
 # 创建一个日志记录器
-logger = logging.getLogger('my_logger')
+logger = logging.getLogger(__name__)
 handler = logging.StreamHandler()
 
 # 设置自定义的颜色格式化器
@@ -57,26 +58,63 @@ def is_eyes_closed(eye):
     return ear < EAR_THRESHOLD
 
 
+def get_image(file_path):
+    if file_path.lower().endswith(('.jpg', '.jpeg', '.png')):
+        return cv2.imread(file_path)
+    if file_path.lower().endswith('.cr3'):
+        with rawpy.imread(file_path) as raw:
+            return raw.postprocess()
+    logger.debug(f'not supported file type: {file_path}')
+    return None
+
+
+def resize_image(image, max_width=1920, max_height=1080):
+    # 获取图像的高度和宽度
+    height, width = image.shape[:2]
+
+    # 如果图像的宽高都小于等于最大宽高，则不压缩
+    if width <= max_width and height <= max_height:
+        return image
+
+    # 计算缩放比例
+    scale = min(max_width / width, max_height / height)
+
+    # 计算新的宽度和高度
+    new_width = int(width * scale)
+    new_height = int(height * scale)
+
+    logger.info(f'resize image {width}x{height} to {new_width}x{new_height}, scale={scale}')
+
+    # 调整图像大小
+    resized_image = cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_AREA)
+    return resized_image
+
+
 def process_image(file_path):
-    # 读取图像
-    with rawpy.imread(file_path) as raw:
-        image = raw.postprocess()
-    scale_percent = 20  # 缩小到50%
-    width = int(image.shape[1] * scale_percent / 100)
-    height = int(image.shape[0] * scale_percent / 100)
-    dim = (width, height)
-    image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+    image = get_image(file_path)
+    if image is None:
+        logger.debug(f"Cannot read image: {file_path}")
+        return -1
+    image = resize_image(image)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = detector(gray, 1)
     face_num = len(faces)
     if face_num == 0:
         logger.info(f'No face detected: {file_path}')
-        return
+        return -2
     if face_num >= 3:
         logger.warning(f'More than 2 face detected, not support: {file_path}');
-        return
+        return -3
 
-    has_eye_close = False
+    has_eye_close = has_any_eye_close(faces, gray)
+
+    logger.info(f'Close eye {"" if has_eye_close else "not "}detected: {file_path}')
+    if has_eye_close:
+        logger.info(f'Eye closed, will mark {file_path} score as 1')
+        mark_score_1(file_path)
+    return 1 if has_eye_close else 0
+
+def has_any_eye_close(faces, gray):
     for face in faces:
         shape = predictor(gray, face)
         shape = face_utils.shape_to_np(shape)
@@ -87,13 +125,8 @@ def process_image(file_path):
         left_closed = is_eyes_closed(leftEye)
         right_closed = is_eyes_closed(rightEye)
         if left_closed or right_closed:
-            has_eye_close = True
-            break
-
-    logger.info(f'Close eye {"" if has_eye_close else "not "}detected: {file_path}')
-    if has_eye_close:
-        logger.info(f'Eye closed, will mark {file_path} score as 1')
-        mark_score_1(file_path)
+            return True
+    return False
 
 
 def mark_score_1(photo_path):
@@ -101,14 +134,43 @@ def mark_score_1(photo_path):
     xmp_file = os.path.splitext(photo_path)[0] + '.xmp'
     shutil.copy2('templates/mark1.xmp', xmp_file)
 
+def scan_directory_and_score_images(directory):
+    results = {}
+    # 扫描指定目录及其子目录
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if not file.lower().endswith(('.jpg', '.jpeg', '.png', '.cr3')):
+                continue
+            file_path = os.path.join(root, file)
+            logger.debug(f"process {file_path} start")
+            results[file_path] = process_image(file_path)
+    return results
 
 def main(directory):
     # 遍历目录中的所有JPG文件
-    for filename in os.listdir(directory):
-        if filename.lower().endswith('.cr3'):
-            file_path = os.path.join(directory, filename)
-            logger.debug(f"process {file_path} start")
-            process_image(file_path)
+    results = scan_directory_and_score_images(directory)
+    # 映射结果代码到错误原因
+    error_reasons = {
+        0: "未检测到闭眼",
+        1: "检测到闭眼",
+        -1: "图像读取失败",
+        -2: "没有检测到人脸",
+        -3: "大于2张人脸"
+    }
+
+    # 统计每种结果的个数
+    result_counts = Counter(results.values())
+
+    # 计算总数
+    total_count = len(results)
+
+    # 打印每种结果的个数和占比
+    logger.info("结果统计:")
+    for result in sorted(result_counts.keys()):  # 排序结果代码
+        count = result_counts[result]
+        percentage = (count / total_count) * 100  # 计算占比
+        reason = error_reasons.get(result, "未知原因")  # 获取对应的错误原因
+        logger.info(f"结果 {result} ({reason}): 个数 = {count}, 占比 = {percentage:.2f}%")
 
 
 if __name__ == "__main__":
